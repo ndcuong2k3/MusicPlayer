af3e29ead56c26cec7e9ccdfe07f8525d0746e87
ndcuong2k3 <147080251+ndcuong2k3@users.noreply.github.com>
2023-12-16 13:53:32 +0700

    Update song.js

---
+++ song.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name:'Hành khúc Đại học mở hà nội',
+        path:'music/hkdhm.mp3',
+        artist:'Lynk Lee - Bùi Hoàng Yến',
+        image:'image/houimg.jpg'
+    },
+    {
+        name:'À Lôi',
+        path:'music/ALoi.mp3',
+        artist:'Double 2T',
+        image:'image/aloi.jpg'
+    },
+    {
+        name:'Nàng',
+        path:'music/nang.mp3',
+        artist:'Ogenus',
+        image:'image/nang.jpg'
+    },
+    {
+        name:'Querry',
+        path:'music/querry.mp3',
+        artist:'QNT X Trung Trần X MCK',
+        image:'image/qnt.jpg'
+    },
+    
+];
